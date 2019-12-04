fad8558945ec68a7853a7a0332c74840ac77553d
Lior <lior_s99@outlook.com>
2019-12-03 19:59:43 -0700

    added more stuff to edit employee details

--- admin/admin_emp_details.php
+++ admin/admin_emp_details.php
@@ -4,9 +4,20 @@
     require("admin_session.php");
 
 
-    if(isset($_POST["submit"])){
-        
-    }
+    $fname_error="";
+    $lname_error="";
+    $email_error="";
+    $phone_error="";
+    $jobType_error="";
+    $startDate_error="";
+    $wage_error="";
+    
+    $street_error="";
+    $houseNum_error="";
+    $city_error="";
+    $postal_error="";
+
+
 
     //Check GET request ID parameter
     if(isset($_GET["id"])){
@@ -16,27 +27,225 @@
 
         $res = getQueryResults($sql);
 
-        $emp_id = $res[0]["Employee_ID"];
-        $emp_fn = $res[0]["FName"];
-        $emp_ln = $res[0]["LName"];
-        $emp_phone = $res[0]["phone_number"];
-        $emp_jobtype = $res[0]["job_type"];
-        $emp_startdate = $res[0]["start_date"];
-        $emp_wage = $res[0]["hourly_wage"];
-        $emp_email = $res[0]["email"];
-
-
-        $sql = "SELECT * FROM admin WHERE Employee_ID = '$id'";
-
-
-        print_r( sizeof(getQueryResults($sql)) );
+        if(isset($_POST["submit"])){
         
-        if( sizeof(getQueryResults($sql))!=0 ){
-            $isAdmin = "Admin";
+            $emp_id = $res[0]["Employee_ID"];
+            $emp_fn = $_POST["fname"];
+            $emp_ln = $_POST["lname"];
+            $emp_phone = $_POST["phone"];
+            $emp_jobtype = $_POST["jobType"];
+            $emp_startdate = $_POST["startDate"];
+            $emp_wage = $_POST["wage"];
+            $emp_email = $_POST["email"];
+
+            $emp_street= $_POST["street"];
+            $emp_house = $_POST["house_num"];
+            $emp_city= $_POST["city"];
+            $emp_postal= $_POST["postal"];
+
+
+            echo $emp_fn;
+            $isValid = true;
+            
+            if(empty($_POST["fname"])){
+                $fname_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $fname = $_POST["fname"];
+                if(!preg_match('/^[a-z,A-Z,\s]*$/', $_POST["fname"])){
+                    $fname_error="This field must contain only letters";
+                    $isValid=false;
+                }
+            }
+            if(empty($_POST["lname"])){
+                $lname_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $lname = $_POST["lname"];
+                if(!preg_match('/^[a-z,A-Z,\s]*$/', $_POST["lname"])){
+                    $lname_error="This field must contain only letters";
+                    $isValid=false;
+                }
+    
+            }
+    
+            if(empty($_POST["phone"])){
+                $phone_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $phone = $_POST["phone"];
+                if(!preg_match("/^[0-9]*$/", $_POST["phone"])){
+                    $phone_error="Please provide a valid phone number";
+                    $isValid=false;
+                }
+            }
+    
+            if(empty($_POST["jobType"])){
+                $jobType_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $job_type = $_POST["jobType"];
+                if(!preg_match('/^[a-z,A-Z,\s]*$/', $_POST["jobType"])){
+                    $jobType_error="This field must contain only letters";
+                    $isValid=false;
+                }
+            }
+
+            if(empty($_POST["wage"])){
+                $phone_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $wage = $_POST["wage"];
+                if(!preg_match("/^[0-9]*$/", $_POST["phone"])){
+                    $phone_error="Please provide a valid wage";
+                    $isValid=false;
+                }
+            }
+    
+            if(empty($_POST["email"])){
+                $email_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $email = $_POST["email"];
+                if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+                    $email_error="Email is not in the correct form!";
+                    $isValid=false;
+                }
+                
+            }
+            if(empty($_POST["startDate"])){
+                $startDate_error="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $start_date = $_POST["startDate"];
+            }
+    
+            if(empty($_POST["street"])){
+                $street_error ="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $street = $_POST["street"];
+                if(!preg_match('/^[a-z,A-Z,\s]*$/', $_POST["street"])){
+                    $street_error="This field must contain only letters";
+                    $isValid=false;
+                }
+            }
+            
+            if(empty($_POST["house_num"])){
+                $houseNum_error ="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $house_num = $_POST["house_num"];
+                if(!preg_match("/^[0-9]*$/", $_POST["house_num"])){
+                    $houseNum_error="Please provide a valid house number";
+                    $isValid=false;
+                }
+            }
+    
+            if(empty($_POST["city"])){
+                $city_error ="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $city = $_POST["city"];
+                if(!preg_match('/^[a-z,A-Z,\s]*$/', $_POST["city"])){
+                    $city_error="This field must contain only letters";
+                    $isValid=false;
+                }
+            }
+    
+            if(empty($_POST["postal"])){
+                $postal_error ="This field cannot be empty";
+                $isValid=false;
+            }
+            else{
+                $postal = $_POST["postal"];
+                if(!preg_match("/^[a-z,A-Z][0-9][a-z,A-Z][0-9][a-z,A-Z][0-9]$/", $_POST["postal"])){
+                    $postal_error="Please provide a valid postal number";
+                    $isValid=false;
+                }
+            }
+
+            if($isValid){
+                echo "INPUT IS VALID";
+
+                //Update basic attributes
+                $sql = "UPDATE employee SET 
+                        FName = '$fname',
+                        LName = '$lname',
+                        phone_number = '$phone',
+                        job_type = '$job_type',
+                        start_date = '$start_date',
+                        hourly_wage = '$wage',
+                        email = '$email',
+                        adr_street = '$street',
+                        adr_housenumber = '$house_num',
+                        adr_city = '$city',
+                        adr_postalcode = '$postal'
+
+                        WHERE Employee_ID = '$id'
+                    ";
+
+                execQuery($sql);
+
+                if($_POST["position"]=="emp"){
+                    //Check if we are "demoting the user"
+                    $sql = "SELECT * FROM admin where Employee_ID='$id'";
+                    if( sizeof(getQueryResults($sql))!=0 ){
+                        $sql = "DELETE  FROM admin WHERE Employee_ID='$id'";
+                        execQuery($sql);
+                    }
+                }
+
+                elseif($_POST["position"]=="adm"){
+                    //Check if we are "promoting the user"
+                    $sql = "SELECT * FROM admin where Employee_ID='$id'";
+                    if( sizeof(getQueryResults($sql))==0 ){
+                        $sql = "INSERT INTO admin  VALUES ('$id', 'some time')"; //NEED TO CHANGE THE ADMIN TITLE
+                        execQuery($sql);
+                    }
+
+                }
+
+            }
         }
+
         else{
-            $isAdmin="Employee";
+            $emp_id = $res[0]["Employee_ID"];
+            $emp_fn = $res[0]["FName"];
+            $emp_ln = $res[0]["LName"];
+            $emp_phone = $res[0]["phone_number"];
+            $emp_jobtype = $res[0]["job_type"];
+            $emp_startdate = $res[0]["start_date"];
+            $emp_wage = $res[0]["hourly_wage"];
+            $emp_email = $res[0]["email"];
+
+            $emp_house = $res[0]["adr_housenumber"];
+            $emp_street= $res[0]["adr_street"];
+            $emp_city= $res[0]["adr_city"];
+            $emp_postal= $res[0]["adr_postalcode"];
+
         }
+
+    $sql = "SELECT * FROM admin WHERE Employee_ID = '$id'";
+
+    if( sizeof(getQueryResults($sql))!=0 ){
+        $isAdmin = true;
+    }
+    else{
+        $isAdmin=false;
+    }
+
+        
 
        
     }
@@ -53,31 +262,55 @@
     <?php if($res): ?>
 
         <form class ="white" action="admin_emp_details.php?id=<?php echo $id?>" method="POST"> 
-            <label > Fist Name: </label>
-            <input type="text" name="fname" value=<?php echo $emp_fn;?> >
 
+            <label> First Name: </label>
+            <input type="text" name="fname" value='<?php echo $emp_fn;?>'>
+            <div class="red-text"> <?php echo $fname_error;?></div>
+            
             <label > Last Name: </label>
-            <input type="text" name="lname" value=<?php echo $emp_ln;?>>
+            <input type="text" name="lname" value='<?php echo $emp_ln;?>'>
+            <div class="red-text"> <?php echo $lname_error;?></div>
 
             <label > Phone Number: </label>
             <input type="text" name="phone" value=<?php echo $emp_phone;?>>
+            <div class="red-text"> <?php echo $phone_error;?></div>
 
             <label > Email: </label>
             <input type="text" name="email" value=<?php echo $emp_email;?>>
+            <div class="red-text"> <?php echo $email_error;?></div>
 
             <label > Job Type: </label>
-            <input type="text" name="jobType" value=<?php echo $emp_jobtype;?>>
+            <input type="text" name="jobType" value='<?php echo $emp_jobtype;?>'>
+            <div class="red-text"> <?php echo $jobType_error;?></div>
 
             <label > Hourly Wage: </label>
             <input type="text" name="wage" value=<?php echo $emp_wage;?>>
+            <div class="red-text"> <?php echo $wage_error;?></div>
 
             <label > Start date: </label>
             <input type="text" name= "startDate" class="datepicker" value=<?php echo $emp_startdate;?>>
+            <div class="red-text"> <?php echo $startDate_error;?></div>
+
+            <label > House number: </label>
+            <input type="text" name="house_num" value=<?php echo $emp_house;?>>
+            <div class="red-text"> <?php echo $houseNum_error;?></div>
+
+            <label > Street Address: </label>
+            <input type="text" name="street" value='<?php echo $emp_street;?>'>
+            <div class="red-text"> <?php echo $street_error;?></div>
+
+            <label > City: </label>
+            <input type="text" name="city" value='<?php echo $emp_city;?>'>
+            <div class="red-text"> <?php echo $city_error;?></div>
+
+            <label > Postal Code: </label>
+            <input type="text" name="postal" value=<?php echo $emp_postal;?>>
+            <div class="red-text"> <?php echo $postal_error;?></div>
 
             <label>Select position: </label>
-            <select class="browser-default">
-                <option value="1">Employee </option>
-                <option value="2">Admin</option>
+            <select class="browser-default" name="position">
+                <option value="emp" <?php if(!$isAdmin){ echo ("selected"); }?>>Employee </option>
+                <option value="adm" <?php if($isAdmin){ echo ("selected"); }?>>Admin </option>
             </select>
 
             <div class="center">
