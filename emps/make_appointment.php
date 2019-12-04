a887f6e0542cb42458f0fbd449930d62f6cef40c
kickingaustin <notelvisp@gmail.com>
2019-12-03 23:52:46 -0700

    time pickers and Lior form format

--- emps/make_appointment.php
+++ emps/make_appointment.php
@@ -1,37 +1,106 @@
 <?php
 require("../config/db_connect.php");
-$props=["Appointment date" => "", "Start Time" => "", "End Time" => ""];
+
+
+$apptDate_error="";
+$startTime_error="";
+$endTime_error="";
+$apptDate= "";
+$startTime = "";
+$endTime = "";
+
+$currentDate = date('m-d-Y');
+echo $currentDate;
+
+    if(isset($_POST["submit"])){
+    
+        $apptDate = mysqli_real_escape_string($connection, $_POST["startDate"]);
+        $startTime = mysqli_real_escape_string($connection, $_POST["startTime"]);
+        $endTime = mysqli_real_escape_string($connection, $_POST["endTime"]);
+        $isValid = true;
+
+        if(empty($_POST["startDate"])){
+            $apptDate_error="This field cannot be empty";
+            $isValid=false;
+        }
+        else{
+            $apptDate = $_POST["startDate"];
+        }
+        
+        if(empty($_POST["startTime"])){
+            $startTime_error="This field cannot be empty";
+            $isValid=false;
+        }
+        else{
+            $startTime = $_POST["startTime"];
+        
+        }if(empty($_POST["endTime"])){
+            $endTime_error="This field cannot be empty";
+            $isValid=false;
+        }
+        else{
+            $endTime = $_POST["endTime"];
+        }
+
+        if($isValid){
+            echo "INPUT IS VALID";
+            //Insert basic attributes
+            $sql = "INSERT INTO `appointment` (`Employee_ID`, `appt_date`, `start_time`, `end_time`, `date_requested`, `approved_flag`)
+            VALUES ('$id', '$apptDate', '$startTime', '$endTime', '$currentDate', '-1')";
+            execQuery($sql);
+        }
+    }
+
+
 ?>
 <html>
 
-      <script type="text/JavaScript">
-        $(document).ready(function(){
-        $('.datepicker').datepicker();
-      });
-
-      </script>
-
-
     <?php include ("logged_emp_header.php");  ?>
+    
     <section class= "container grey-text">
         <h4 class="center"> Create Appointment </h4>
-    <form class ="white" action="login.php" method="POST">
 
-        <label > Appointment Date: </label>
-            <input type="text" class="datepicker">
+        <form class ="white" action="make_appointment.php?id=<?php echo $id?>" method="POST"> 
 
-        <label > Start Time: </label>
-            <input type="text" name="id" value=<?php echo $props["Start Time"];?> >
-        
-        <label > End Time: </label>
-            <input type="text" name="id" value=<?php echo $props["End Time"];?> >
+            <label > Appointment date: </label>
+            <input type="text" name= "startDate" class="datepicker" value=<?php echo $apptDate;?>>
+            <div class="red-text"> <?php echo $apptDate_error;?></div>
 
-        <div class ="center"> <input type="submit" value="Create Request" class = "btn brand z-depth-0"> </div>
-        <!- An employee ID, date_requested, and approved flag are also need to make the sql insert ->
-        <br>
+            <label > Start Time: </label>
+            <input type="text" name= "startTime" class="timepicker" value=<?php echo $startTime;?>>
+            <div class="red-text"> <?php echo $startTime_error;?></div>
+
+            <label > End Time: </label>
+            <input type="text" name= "endTime" class="timepicker1" value=<?php echo $endTime;?>>
+            <div class="red-text"> <?php echo $endTime_error;?></div>
+
+            <div class ="center"> <input type="submit" value="Create Request" class = "btn brand z-depth-0"> </div>
         </form>
     </section>
 
+
+      <!-- Script for calendar view -->
+ 
+    <script>
+        const Calendar = document.querySelector('.datepicker');
+            M.Datepicker.init(Calendar, {
+                format: 'yyyy-mm-dd'
+
+            });
+    </script>
+    <script>
+         Clock = document.querySelector('.timepicker');
+            M.Timepicker.init(Clock, {twelveHour: false
+            });
+    
+    </script>
+    <!-  They need to be seperate to allow for multiple pickers ->
+    <script>
+         Clock = document.querySelector('.timepicker1');
+            M.Timepicker.init(Clock, {twelveHour: false
+            });
+    </script>
+ 
 
     <?php include('../templates/footer.php'); ?>
 </html>
--- emps/make_vacation.php
+++ emps/make_vacation.php
@@ -1,35 +1,85 @@
 <?php
 require("../config/db_connect.php");
-    $props=["Vacation Start" => "", "Vacation End" => ""];
-?>
 
+$startDate_error="";
+$endDate_error="";
+
+$startDate= "";
+$endDate = "";
+
+$currentDate = date('m-d-Y');
+echo $currentDate;
+
+
+    if(isset($_POST["submit"])){
+    
+        $startDate = mysqli_real_escape_string($connection, $_POST["startDate"]);
+        $endDate = mysqli_real_escape_string($connection, $_POST["endDate"]);
+        $isValid = true;
+
+        if(empty($_POST["startDate"])){
+            $startDate_error="This field cannot be empty";
+            $isValid=false;
+        }
+        else{
+            $startDate = $_POST["startDate"];
+        }
+        
+        if(empty($_POST["endDate"])){
+            $endDate_error="This field cannot be empty";
+            $isValid=false;
+        }
+        else{
+            $endDate = $_POST["endDate"];
+        }
+
+        if($isValid){
+            echo "INPUT IS VALID";
+            //Insert basic attributes
+            $sql = "INSERT INTO `vacation` (`Employee_ID`, `start_date`, `end_date`, `date_requested`, `approved_flag`)
+             VALUES ('$id', '$startDate', '$endDate', '$currentDate', '-1')";
+            execQuery($sql);
+        }
+    }
+
+
+?>
 <html>
 
-      <script type="text/JavaScript">
-        $(document).ready(function(){
-        $('.datepicker').datepicker();
-      });
-
-      </script>
-
-
-    <?php include('logged_emp_header.php'); ?>
+    <?php include ("logged_emp_header.php");  ?>
+    
     <section class= "container grey-text">
-        <h4 class="center"> Create Vacation </h4>
-    <form class ="white" action="login.php" method="POST">
+        <h4 class="center"> Create Appointment </h4>
 
-        <label >Vacation Start: </label>
-            <input type="text" class="datepicker">
+        <form class ="white" action="make_vacation.php?id=<?php echo $id?>" method="POST"> 
 
-        <label >Vacation End: </label>
-            <input type="text" class="datepicker">
+            <label > Start date: </label>
+            <input type="text" name= "startDate" class="datepicker" value=<?php echo $startDate;?>>
+            <div class="red-text"> <?php echo $startDate_error;?></div>
 
-        <div class ="center"> <input type="submit" value="Create Request" class = "btn brand z-depth-0"> </div>
-        <!- An employee ID, date_requested, and approved flag are also need to make the sql insert ->
-        <br>
+            <label > End date: </label>
+            <input type="text" name= "startDate" class="datepicker1" value=<?php echo $endDate;?>>
+            <div class="red-text"> <?php echo $endDate_error;?></div>
+
+            <div class ="center"> <input type="submit" value="Create Request" class = "btn brand z-depth-0"> </div>
         </form>
     </section>
 
+
+      <!-- Script for calendar view -->
+    <script>
+         Calendar = document.querySelector('.datepicker');
+            M.Datepicker.init(Calendar, {
+                format: 'yyyy-mm-dd'
+
+            });
+    </script>
+     <script>
+         Calendar = document.querySelector('.datepicker1');
+            M.Datepicker.init(Calendar, {
+                format: 'yyyy-mm-dd'
+            });
+    </script>
 
     <?php include('../templates/footer.php'); ?>
 </html>
